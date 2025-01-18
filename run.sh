3a4f4103437a0f13b17d0fe50c4280c5699efd63
pman <pman-$-432894328923@gmail.com>
2025-01-18 13:47:15 +0100

    ...

--- run.sh
+++ run.sh
@@ -9,23 +9,27 @@ UNENCRYPTED_FILE="unencrypted.data"
 ENCRYPTION_ID="encryption-id-187#"
 
 # conditionally unencrypt if encryption id is present
-if [ -f "$ENCRYPTED_FILE" ]; then
-  start=$(head -c "${#ENCRYPTION_ID}" "$ENCRYPTED_FILE")
-fi
+function decrypt() {
+  if [ -f "$ENCRYPTED_FILE" ]; then
+    start=$(head -c "${#ENCRYPTION_ID}" "$ENCRYPTED_FILE")
+  fi
 
-if [[ "$start" == "$ENCRYPTION_ID" ]]; then
-  echo "$ENCRYPTED_FILE is encrypted with $ENCRYPTION_ID"
-  echo "Unencrypting it..."
-  gpg --batch --yes --passphrase "$GPG_PASSPHRASE" --armor --output "$UNENCRYPTED_FILE" --decrypt "$ENCRYPTED_FILE"
-  cat "$UNENCRYPTED_FILE"
-else
-  echo "File is not encrypted...skipping"
-fi
+  if [[ "$start" == "$ENCRYPTION_ID" ]]; then
+    echo "$ENCRYPTED_FILE is encrypted with $ENCRYPTION_ID"
+    echo "Unencrypting it..."
+    gpg --batch --yes --passphrase "$GPG_PASSPHRASE" --armor --output "$UNENCRYPTED_FILE" --decrypt "$ENCRYPTED_FILE"
+    cat "$UNENCRYPTED_FILE"
+  else
+    echo "File is not encrypted...skipping"
+  fi
+}
 
 # encrypt
-echo "$ENCRYPTION_ID" > "$ENCRYPTED_FILE"
-gpg --batch --yes --passphrase "$GPG_PASSPHRASE" --armor --output tmp --symmetric --cipher-algo AES256 "$UNENCRYPTED_FILE"
-cat tmp >> "$ENCRYPTED_FILE"
+function encrypt() {
+  echo "$ENCRYPTION_ID" > "$ENCRYPTED_FILE"
+  gpg --batch --yes --passphrase "$GPG_PASSPHRASE" --armor --output tmp --symmetric --cipher-algo AES256 "$UNENCRYPTED_FILE"
+  cat tmp >> "$ENCRYPTED_FILE"
+}
 
 cat "$ENCRYPTED_FILE" "$UNENCRYPTED_FILE"
 
